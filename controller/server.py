4d14a711f9799a5fc4e19c9563ff02cb1db78890
sinsky <sinsky@magazone.com>
2020-06-11 15:48:19 +0900

    add delete handler

--- controller/server.py
+++ controller/server.py
@@ -2,13 +2,13 @@ import kopf
 
 
 @kopf.on.create('kubeaction.spaceone.dev', 'v1', 'flows')
-def create_fn(body, spec, name, namespace, logger, **kwargs):
+def create(body, spec, name, namespace, logger, **kwargs):
     event = spec.get('on')
     jobs = spec.get('jobs')
     meta = spec.get('meta', {})
     if meta:
         repo = meta.get('repo')
-        spaceone_meta = meta.get('spaceone',{})
+        spaceone_meta = meta.get('spaceone', {})
         domain_id = spaceone_meta.get('domain_id')
 
     if not event:
@@ -17,3 +17,16 @@ def create_fn(body, spec, name, namespace, logger, **kwargs):
         raise kopf.PermanentError("must set more than one job")
 
     kopf.info(body, reason='Start', message='Create Flow')
+
+
+@kopf.on.delete('kubeaction.spaceone.dev', 'v1', 'flows')
+def delete(body, spec, name, namespace, logger, **kwargs):
+    event = spec.get('on')
+    jobs = spec.get('jobs')
+    meta = spec.get('meta', {})
+    if meta:
+        repo = meta.get('repo')
+        spaceone_meta = meta.get('spaceone', {})
+        domain_id = spaceone_meta.get('domain_id')
+
+    kopf.info(body, reason='Start', message='Delete Flow')
