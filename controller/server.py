cefdfaa5e3e91da6c3ffe2e2538615db5ca6e699
sinsky <sinsky@magazone.com>
2020-06-11 13:45:54 +0900

    log when resource create

--- controller/server.py
+++ controller/server.py
@@ -1,12 +1,8 @@
-import os
-
 import kopf
-import kubernetes
-import yaml
 
 
 @kopf.on.create('kubeaction.sapceone.dev', 'v1', 'flow')
-def create_fn(spec, name, namespace, logger, **kwargs):
+def create_fn(body, spec, name, namespace, logger, **kwargs):
     event = spec.get('on')
     jobs = spec.get('jobs')
     meta = spec.get('meta', {})
@@ -19,15 +15,4 @@ def create_fn(spec, name, namespace, logger, **kwargs):
     if not jobs or len(jobs) < 1:
         raise kopf.PermanentError("must set more than one job")
 
-    path = os.path.join(os.path.dirname(__file__), 'pvc.yaml')
-    tmpl = open(path, 'rt').read()
-    text = tmpl.format(name=name, size=size)
-    data = yaml.safe_load(text)
-
-    api = kubernetes.client.CoreV1Api()
-    obj = api.create_namespaced_persistent_volume_claim(
-        namespace=namespace,
-        body=data,
-    )
-
-    logger.info(f"PVC child is created: %s", obj)
+    kopf.info(body, reason='Start', message='Create Flow')
